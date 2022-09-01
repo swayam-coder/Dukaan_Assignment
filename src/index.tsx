de853915449d9ee5d5a837902fe208613a8b4983
swayam-coder <swayam14feb@gmail.com>
2022-09-01 11:55:30 +0000

    Bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import * as ReactDOMClient from "react-dom/client";
 
 import App from "./App";
@@ -7,7 +6,5 @@ const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement);
 
 root.render(
-  <StrictMode>
     <App />
-  </StrictMode>
 );
